690bff8dc3dc58a728cf3823177b29a6cdd19386
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-18 18:19:21 +0300

    change comment

--- src/a/b/c/Foo3.java
+++ src/a/b/c/Foo3.java
@@ -3,7 +3,7 @@ package a.b.c;
 /**
  * *******************************
  * Created by Irina.Petrovskaya on 5/18/2018.
- * Project: r3_clone1
+ * Project: r3_clone2
  * *******************************
  */
 public class Foo3 {
