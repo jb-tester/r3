f58ec6056997b6756c6ae4a5dcf771af16932693
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-05-18 18:38:10 +0300

    edit comments

--- src/a/b/c/Foo3.java
+++ src/a/b/c/Foo3.java
@@ -3,6 +3,7 @@ package a.b.c;
 /**
  * *******************************
  * Created by Irina.Petrovskaya on 5/18/2018.
+ *
  * Project: r3_clone1 (first clone of r3)
  * *******************************
  */
