0f15b1089530d00887d4170b657e6fb79f481888
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-12-06 19:53:16 +0300

    update my name

--- src/a/b/c/Foo3.java
+++ src/a/b/c/Foo3.java
@@ -2,7 +2,7 @@ package a.b.c;
 
 /**
  * *******************************
- * Created by Irina.Petrovskaya on 5/18/2018.
+ * Created by Irina Petrovskaya on 5/18/2018.
  * Project: r3_clone1 (first clone of r3)
  * *******************************
  */
